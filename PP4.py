3e909cae5636e5858de5c7a989587d59bc69fa50
Lyubomyr21 <yupiter2101@gmail.com>
2020-11-12 23:50:48 +0200

    VovchakCommit

--- PP4.py
+++ PP4.py
@@ -1,1 +1,12 @@
-print("git")
+from flask import Flask
+from wsgiref.simple_server import make_server
+app = Flask(__name__)
+
+@app.route('/api/v1/hello-world-6')
+def hello_world():
+    return 'Hello, World 6!'
+
+with make_server('',228,app)as server:
+    print("Port 127.0.0.1:228")
+
+    server.serve_forever()
